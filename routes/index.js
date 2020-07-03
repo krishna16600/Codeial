fc1cf0c9a93f63fe47f60b4a4fda4608c760aa11
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-03 14:17:14 +0530

    added router and controller for home

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+
+
+router.get('/', homeController.home)
+
+
+
+
+//It will export all the routes
+module.exports = router;
