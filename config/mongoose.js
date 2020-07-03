8f9a82673f37fbe7400882aa6634630e2314b462
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-03 15:50:50 +0530

    setting up MongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/codeial_development');
+
+
+const db = mongoose.connection;
+
+db.on('error' , console.error.bind(console , 'Error connecting to db'));
+
+db.once('open' , () => {
+    console.log('Connected to DB :: MongoDB');
+    
+})
+
+module.exports = db;
