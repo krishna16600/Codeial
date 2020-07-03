b6f3525e03249281d7ee19c5f874ff00cc110105
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-03 15:43:34 +0530

    setting up static files

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+
+//use static files.. css and js
+
+app.use(express.static("./assets"));
 app.use(expressLayouts);
+
+//extract styles and css from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 
 //use express router
