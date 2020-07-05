9b6f1257309397c5a9e6f70db1764e81a4ef2954
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-05 23:32:28 +0530

    User can now delete the comment on his post and others

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,3 +24,30 @@ module.exports.create = (req,res) => {
         }
     })    
 }
+
+module.exports.destroy = (req,res) => {
+    Comment.findById(req.params.id , (err,comment) => {
+
+        //if the same user which has commented,is logged in. then delete it!
+        if(comment.user == req.user.id){
+            
+            /*upon finding the required comment, we need to go into post schema as well, in that particular post's array of comments,
+            we need to find the same comment id and delete it from there as well
+            So overall, we delete Comment from comment collection and comment id from Post document's comment array.
+            */
+
+            let postId = comment.post;
+
+            comment.remove();
+
+            //find post by postId and pull out comments array and update the comments array by removing the id
+            Post.findByIdAndUpdate(postId , {
+                $pull: {comments: req.params.id}
+            } , (err, post) => {
+                return res.redirect('back');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    })
+}
