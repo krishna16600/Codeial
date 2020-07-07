5a4224a23c1ffc8c30d7200798d6a36f5bad3224
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-07 18:24:52 +0530

    Clean coding practice by using async-await functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,52 +2,60 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 
 
-module.exports.create = (req,res) => {
-    console.log(req.body.post);
-    
-    Post.findById(req.body.post, (err,post)=> {
-        if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            } , (err, comment) => {
-                if(err){
-                    //handle error if comment not created
-                    
-                }
-                post.comments.push(comment);
-                post.save();
+module.exports.create = async (req,res) => {
 
-                res.redirect('/');
-            })
+    try{
+    let post = await Post.findById(req.body.post); 
+
+    if(post){
+        let comment = await Comment.create({
+            content: req.body.content,
+            post: req.body.post,
+            user: req.user._id
+        });
+
+            post.comments.push(comment);
+            post.save();
+
+            res.redirect('/');
         }
-    })    
-}
+    } catch(err) {
+        console.log(`Error in adding comment, ${err}`);
+    }
+}   
 
-module.exports.destroy = (req,res) => {
-    Comment.findById(req.params.id , (err,comment) => {
-
-        //if the same user which has commented,is logged in. then delete it!
-        if(comment.user == req.user.id){
+module.exports.destroy = async (req,res) => {
+     
+    try{
+            let comment = await Comment.findById(req.params.id);
+            let post = await Post.findById(comment.post);
             
-            /*upon finding the required comment, we need to go into post schema as well, in that particular post's array of comments,
-            we need to find the same comment id and delete it from there as well
-            So overall, we delete Comment from comment collection and comment id from Post document's comment array.
-            */
+            //if the same user which has commented,is logged in. then delete it!
+            if(comment.user == req.user.id || post.user == req.user.id){
+                
+                
+                /*upon finding the required comment, we need to go into post schema as well, in that particular post's array of comments,
+                we need to find the same comment id and delete it from there as well
+                So overall, we delete Comment from comment collection and comment id from Post document's comment array.
+                */
 
-            let postId = comment.post;
+                let postId = comment.post;
 
-            comment.remove();
+                comment.remove();
 
-            //find post by postId and pull out comments array and update the comments array by removing the id
-            Post.findByIdAndUpdate(postId , {
-                $pull: {comments: req.params.id}
-            } , (err, post) => {
+                //find post by postId and pull out comments array and update the comments array by removing the id
+                 let post =  Post.findByIdAndUpdate(postId , {
+                    $pull: {comments: req.params.id}
+                });
+
                 return res.redirect('back');
-            })
-        } else {
-            return res.redirect('back');
-        }
-    })
+
+            } else {
+                return res.redirect('back');
+            }
+        
+    } catch(err) {
+        console.log(`Error in adding comment, ${err}`);
+        return;
+    }
 }
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,67 +3,60 @@ const Comment = require('../models/comment');
 const User = require('../models/user');
 
 
-module.exports.posts = (req,res) => {
+module.exports.posts =  async (req,res) => {
    
-    //populate the user of each post
-    Post.find({}).populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    }) 
-    .exec((err,posts) => {
-
-        User.find({} , (err,users) => {
-            return res.render('home', {
-                title: 'Codeial | News-Feed',
-                posts: posts,
-                all_users: users
-            })
+    try {
+         //populate the user of each post
+        let posts = await Post.find({}).populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        }) 
+        let users = await  User.find({});
+        
+        return res.render('home', {
+            title: 'Codeial | News-Feed',
+            posts: posts,
+            all_users: users
         })
-       
-    })
-
+    } catch(err){
+        console.log(`Error in finding post, ${err}`)
+    }
+  
 }
 
-module.exports.create = (req,res) => {
+module.exports.create = async (req,res) => {
   
 
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, (err,post) => {
-        if(err){
-            console.log(`Error in creating a post, ${err}`);
-            return;
-        }
-        console.log(post);
-        
+    try {
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        })
+
         return res.redirect('back');
-    })
+    } catch(err){
+        console.log(`Error in creating post, ${err}`);
+    }
 }
 
+module.exports.destroy = async (req,res) => {
+    try{
+        let post = await Post.findById(req.params.id);
 
-module.exports.destroy = (req,res) => {
-    Post.findById(req.params.id, (err,post) => {
-        
-        //if found and check if user is same as author
-        //when comparing two id's , we need to convert it into strings, ideally it should be req.user._id but it's an object and we need
-        // to convert it into string. mongoose provides us the functionality and we can simply write as below
         if(post.user == req.user.id){
-                post.remove();
+            post.remove();
 
-                //.deletMany() will delete all records
-                Comment.deleteMany({post: req.params.id} , (err) => {
-                    if(err){
-                        return res.redirect('back');
-                    }
-                })
-
-                return res.redirect('back');
-        }
-        else 
+            //.deletMany() will delete all records
+        await Comment.deleteMany({post: req.params.id});
             return res.redirect('back');
-    })
+        }
+        else{ 
+            return res.redirect('back');
+        }
+    } catch(err){
+        console.log(`Error in creating post, ${err}`);
+    }
 }
